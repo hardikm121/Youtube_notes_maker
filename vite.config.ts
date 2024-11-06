b5a8383584f89a7114fc014622410c097fd4ce88
Hardik Malviya <125384251+hardikm121@users.noreply.github.com>
2024-11-06 15:38:46 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,14 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
+  build: {
+    chunkSizeWarningLimit: 1000, // Increase this limit as needed
   },
   server: {
     port: process.env.PORT || 3000,
     host: '0.0.0.0',
+  },
+  optimizeDeps: {
+    exclude: ['lucide-react'],
   },
 });
