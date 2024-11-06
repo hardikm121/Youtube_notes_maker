547ac757f9f41806ce3b2ce04cd266f42d6ba7f3
Hardik Malviya <125384251+hardikm121@users.noreply.github.com>
2024-11-06 15:33:18 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
+  },
+  server: {
+    port: process.env.PORT || 3000, // default to 3000 if PORT is not set
+    host: '0.0.0.0', // allow access from external IPs (useful for Render)
   },
 });
